5e0b915986e2f78253bcb68a2ca2faf247b1ee8f
Martin <94791875+PomeloPack@users.noreply.github.com>
2023-04-17 17:11:37 +0200

    Add files via upload

---
+++ psswd.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import random
+
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Random PyPassword Generator!")
+
+
+random_number_letters = int(input("How many letters would you like in your password?\n")) 
+random_number_numbers = int(input("How many symbols would you like?\n"))
+random_number_symbols = int(input("How many numbers would you like?\n"))
+
+
+
+password_list = []
+
+
+for letter in range(1, random_number_numbers + 1):
+    password_list.append(random.choice(letters))
+    
+
+for number in range(1, random_number_numbers + 1):
+    password_list.append(random.choice(numbers))
+
+for symbol in range(1, random_number_symbols + 1):
+    password_list.append(random.choice(symbols))
+
+
+random.shuffle(password_list)
+
+
+final_password = ""
+for password in password_list:
+  final_password += password
+
+print(f"Your generated password is: {final_password}")
+
+print("------------------------------------------------------------------------------------------------------------")
+
+passwd_again = input(print(f"If you dont't like your psswd:{final_password} do you want to generate a new one ? [yes / no]: "))
+
+if passswd_again == "yes":
+	print("test")
